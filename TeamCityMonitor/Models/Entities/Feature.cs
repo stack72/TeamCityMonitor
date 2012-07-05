3dd78acd357dc7e91a7c3215a9a3e81b4153406a
stack72 <public@paulstack.co.uk>
2012-07-05 22:20:17 +0100

    deleting the experimental work to do with the admin section

--- TeamCityMonitor/Models/Entities/Feature.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BuildMonitor.Models.Entities
-{
-    public class Feature
-    {
-        public int FeatureSwitchId { get; set; }
-        public string FeatureName { get; set; }
-        public bool Enabled { get; set; }
-    }
-}
